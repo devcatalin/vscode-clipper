7319adc948f4b0a8fe0789fe6ca1b11e7016b25d
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2024-11-01 18:00:18 +0200

    feat: register sidebar view

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,18 @@ import * as vscode from "vscode";
 import { copyFileTree } from "./commands/copyFileTree";
 import { copyFileTreeDepth } from "./commands/copyFileTreeDepth";
 import { copyOpenFiles } from "./commands/copyOpenFiles";
+import { SidebarProvider } from "./SidebarProvider";
 
 export function activate(context: vscode.ExtensionContext) {
+  const sidebarProvider = new SidebarProvider(context.extensionUri);
+
   const commandDisposables = [
     vscode.commands.registerCommand("clipper.copyFileTree", copyFileTree),
     vscode.commands.registerCommand("clipper.copyFileTreeDepth", copyFileTreeDepth),
     vscode.commands.registerCommand("clipper.copyOpenFiles", copyOpenFiles),
+    vscode.window.registerWebviewViewProvider("clipper.cardsView", sidebarProvider),
   ];
+
   context.subscriptions.push(...commandDisposables);
 }
 
